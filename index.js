f0d3e3c0ba68476403b95f2eb1ca35f4a175de77
karol <karol.rudzinski1990@gmail.com>
2022-05-05 18:49:51 +0200

    qweqwe

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ dotenv.config();
 //connect to db
 mongoose.connect(process.env.DB_CONNECT, () => console.log("connected "));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
-
 //middlewares
 app.use(express.json());
 app.use(cors());
